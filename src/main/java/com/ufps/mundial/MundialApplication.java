6afe35d83095de4e220a8b1a3d94b964bee49aa3
Jesus Cristancho <jesusmanuelcp@ufps.edu.co>
2022-11-28 10:22:42 -0500

    repo creado

---
+++ src/main/java/com/ufps/mundial/MundialApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.mundial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufps/mundial/MundialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.mundial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
