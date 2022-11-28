afe0c04967bed3b0ba69be62f411626111f6b4cd
Jesus Cristancho <jesusmanuelcp@ufps.edu.co>
2022-11-28 11:28:56 -0500

    entidades actualizadas

--- src/main/java/com/ufps/mundial/model/Continente.java
+++ src/main/java/com/ufps/mundial/model/Continente.java
@@ -20,7 +20,7 @@ public class Continente {
 
     @Id
     @SequenceGenerator(name="continente_id_seq", allocationSize=1)
-    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="user_id_seq")
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="continente_id_seq")
     private Integer id;
 
     private String nombre;
